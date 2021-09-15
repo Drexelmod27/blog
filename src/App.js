886b8362142b42b1fab3ea23d6379b2894b0d3f9
drexelmod27 <drexelmod27@gmail.com>
2021-09-15 16:55:33 -0700

    started login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import {Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/dashboard" component={Dashboard} />
     </div>
   );
 }
